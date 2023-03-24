9c4afdce688805b9dbc2b37b5d4897b031c6a3ab
gabrielc42 <clark.gabe97@gmail.com>
2023-03-24 15:07:07 -0500

    pub fn new returns Universe

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -61,6 +61,31 @@ fn main() {
             self.cells = next;
         }
 
+        pub fn new() -> Universe {
+            let width = 64;
+            let height = 64;
+
+            let cells = (0..width * height)
+                .map(|i| {
+                    if i % 2 == 0 || i % 7 == 0 {
+                        Cell::Alive
+                    } else {
+                        Cell::Dead
+                    }
+                })
+                .collect();
+
+            Universe {
+                width,
+                height,
+                cells,
+            }
+        }
+
+        pub fn render(&self) -> String {
+            self.to_string()
+        }
+
         fn get_index(&self, row: u32, column: u32) -> usize {
             (row * self.width + column) as usize
         }
