510390e54b0a136f9e046d3b62d21725a768c340
gabrielc42 <clark.gabe97@gmail.com>
2023-03-24 15:02:33 -0500

    impl Display for Universe (fn tick)

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -30,6 +30,37 @@ pub struct Universe {
 
 fn main() {
     impl Universe {
+        pub fn tick(&mut self) {
+            let mut next = self.cells.clone();
+
+            for row in 0..self.height {
+                for col in 0..self.width {
+                    let idx = self.get_index(row, col);
+                    let cell = self.cells[idx];
+                    let live_neighbors = self.live_neighbor_count(row, col);
+
+                    let next_cell = match (cell, live_neighbors) {
+                        // Rule 1: Any live cell with fewer than two live nieghbours
+                        // dies, as if caused by underpopulation.
+                        (Cell::Alive, x) if x < 2 => Cell::Dead,
+                        // Rule 2: Any live cell with two or three live nieghbours
+                        // lives on to the next generation.
+                        (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                        // Rule 3: Any live cell with more than three live
+                        // nieghbours dies, as if by overpopulation.
+                        (Cell::Alive, x) if x > 3 => Cell::Dead,
+                        // Rule 4: Any dead cell with exactly three live nieghbours
+                        // becomes a live cell, as if by reproduction.
+                        (Cell::Dead, 3) => Cell::Alive,
+                        // All other cells remain in the same state.
+                        (otherwise, _) => otherwise,
+                    };
+                    next[idx] = next_cell;
+                }
+            }
+            self.cells = next;
+        }
+
         fn get_index(&self, row: u32, column: u32) -> usize {
             (row * self.width + column) as usize
         }
@@ -49,6 +80,22 @@ fn main() {
                 }
             }
             count
+        }
+    }
+
+    use std::fmt;
+
+    impl fmt::Display for Universe {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            for line in self.cells.as_slice().chunks(self.width as usize) {
+                for &cell in line {
+                    let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                    write!(f, "{}", symbol)?;
+                }
+                write!(f, "\n")?;
+            }
+
+            Ok(())
         }
     }
 }
