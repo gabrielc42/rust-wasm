0ae97045b2a147edeb990764335555ef32479a8d
gabrielc42 <clark.gabe97@gmail.com>
2023-03-27 11:26:47 -0500

    canvas, Javascript & WA memory optimization

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -28,6 +28,7 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
+// public methods, exported to Javascript
 #[wasm_bindgen]
 impl Universe {
     pub fn tick(&mut self) {
@@ -84,6 +85,18 @@ impl Universe {
 
     pub fn render(&self) -> String {
         self.to_string()
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
     }
 }
 
